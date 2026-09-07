4d92dd19d0ce7153f3e53315e352967208498a66
implox <implox.v1@gmail.com>
2026-09-07 21:08:31 +0530

    config and instantiation and loopfucn

--- cache.go
+++ cache.go
@@ -2,15 +2,34 @@ package gocache
 
 import "sync"
 
+type CacheConfig struct {
+	InitialMapCapacity int
+}
+
+var DefaultCacheConfig = CacheConfig{0}
+
 // Cache implementation with generic map and read write mutex
-type Cache[K comparable, V any] struct {
+type Cache[K k, V v] struct {
 	m  map[K]V
 	mu sync.RWMutex
 }
 
-// Instantiates a new cache using the generic types provided to this constructor function
+func newCache[K k, V v](config CacheConfig) Cache[K, V] {
+	return Cache[K, V]{
+		m: make(map[K]V, config.InitialMapCapacity),
+	}
+}
+
+// Instantiates a new cache using the generic types provided to this constructor function and default [DefaultCacheConfig]
 func NewCache[K comparable, V any]() *Cache[K, V] {
-	return &Cache[K, V]{}
+	c := newCache[K, V](DefaultCacheConfig)
+	return &c
+}
+
+// Instantiates a new cache using the config provided and generics
+func NewCacheWithConfig[K comparable, V any](config CacheConfig) *Cache[K, V] {
+	c := newCache[K, V](config)
+	return &c
 }
 
 // Adds a key value pair to the cache
@@ -40,4 +59,21 @@ func (c *Cache[K, V]) Delete(key K) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 	delete(c.m, key)
+}
+
+// Loops over all the items in the list and passes the key and value to the function provided
+//
+// the function must return a uint8, which tells when to quit the loop
+//   - return 0 to exit the loop
+//   - return >= 1 to continue the loop
+//
+// the read lock unlocks itself only after this function call has ended, do not run any other methods on this instance of [Cache] inside the provided function fn
+func (c *Cache[K, V]) LoopFunc(fn func(key K, value V) uint8) {
+	c.mu.RLock()
+	defer c.mu.RUnlock()
+	for k, v := range c.m {
+		if r := fn(k, v); r == 0 {
+			return
+		}
+	}
 }
